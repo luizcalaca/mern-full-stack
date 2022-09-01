48f2aa421deb052db5382726db406a52523584ec
luizcalaca <luizcalaca@gmail.com>
2022-09-01 20:58:34 -0300

    chore: const refactor

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -16,6 +16,6 @@ const postSchema = mongoose.Schema({
     },
 })
 
-var PostMessage = mongoose.model('PostMessage', postSchema);
+const PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
